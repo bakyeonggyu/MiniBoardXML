745c063f835c26e138633d0d4776eb456684a0fb
bakyo <bakyo@DESKTOP-VQLDGQ9>
2020-08-24 18:13:10 +0900

    init

---
+++ src/mn/brd/controller/HomeController.java
@@ -0,0 +1,24 @@
+package mn.brd.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import mn.brd.beans.UserBean;
+
+@Controller
+public class HomeController {
+
+//	@Resource(name = "loginUserBean")
+//	@Lazy
+//	private UserBean loginUserBean;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+//		System.out.println(loginUserBean);
+		return "redirect:/main";
+	}
+}
---
+++ src/mn/brd/interceptor/TopMenuInterceptor.java
@@ -0,0 +1,36 @@
+package mn.brd.interceptor;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import mn.brd.beans.BoardInfoBean;
+import mn.brd.beans.UserBean;
+import mn.brd.service.TopMenuService;
+
+public class TopMenuInterceptor implements HandlerInterceptor{
+
+	@Autowired
+	private TopMenuService topMenuService;
+	
+	@Resource(name = "loginUserBean")
+	@Lazy
+	private UserBean loginUserBean;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		List<BoardInfoBean> topMenuList = topMenuService.getTopMenuList();
+		request.setAttribute("topMenuList", topMenuList);
+		request.setAttribute("loginUserBean", loginUserBean);
+		
+		return true;
+	}
+}
